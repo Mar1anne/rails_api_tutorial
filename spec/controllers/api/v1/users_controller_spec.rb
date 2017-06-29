9a679fa9f307d45f1bd5744452359edbc0be7664
Mariana <mariana.ristovska@gmail.com>
2017-06-29 13:45:14 +0200

    Fixed issue with user specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,26 +1,25 @@
 require 'rails_helper'
-# require 'spec_helper'
 
-RSpec.describe Api::V1::UsersController, type: :controller do
-  before(:each) { request.headers['Accept'] = 'application/vnd.marketplace.v1' }
+RSpec.describe Api::V1::UsersController do
+  include FactoryGirl
+
+  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
 
   describe "GET #show" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      get :show, params: { id: @user.id, format: :json }
+      # get :show, id: @user.id, format: :json
+    end
 
-    # before(:each) do
-    #   @user = FactoryGirl.create :user
-    #   get :show, :id => 1, format: :json
-    # end
-    #
-    # it 'returns information about a reporter on a hash' do
-    #   user_response = JSON.parse(response.body, symbolize_names: true)
-    #   expect(user_response[:email]).to eql @user.email
-    # end
-    #
-    # it { should respond_with 200 }
-    #
-    # it {expect(response).to respond_with 200}
-    #
-    # it { expect(response.status).to eq(200) }
+    it "returns the information about a reporter on a hash" do
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:email]).to eql @user.email
+    end
+
+    it 'responds with status code 200' do
+      expect(response.status).to eql(200)
+    end
 
   end
 end
