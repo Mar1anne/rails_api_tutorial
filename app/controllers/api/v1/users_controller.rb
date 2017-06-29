2658fdf1d692a89b369cf43b1858d8c2c24ea279
Mariana <mariana.ristovska@gmail.com>
2017-06-29 15:07:08 +0200

    Added update action on users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,32 @@ class Api::V1::UsersController < ApplicationController
   def show
     respond_with User.find(params[:id])
   end
+
+  def create
+    user = User.new user_params
+    if user.save
+      render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    Rails.logger.debug("My object: #{user.inspect}")
+
+
+    if user.update(params.permit(:email, :password, :password_confirmation))
+      render json: user, status: 200, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -22,4 +22,74 @@ RSpec.describe Api::V1::UsersController do
     end
 
   end
+
+  describe 'POST #create' do
+    context 'when successfully created' do
+      before(:each) do
+        @user_attr = FactoryGirl.attributes_for(:user)
+        post :create, params: { user: @user_attr, format: :json }
+      end
+
+      it 'renders json representation of user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql(@user_attr[:email])
+      end
+
+      it 'returns 201 status code' do
+        expect(response.status).to eql(201)
+      end
+
+    end
+
+    context 'when not created' do
+      before(:each) do
+        @invalid_user_attr = { password: '1231322', password_confirmation: '1231322' }
+        post :create, params: { user: @invalid_user_attr, format: :json }
+      end
+
+      # it 'renders error json' do
+      #   response = JSON.parse(response.body, symbolize_names: true)
+      #   expect(response[:errors]).not_to be_nil
+      # end
+
+      it 'responds with 422' do
+        expect(response.status).to eql(422)
+      end
+    end
+  end
+
+  describe 'PUT/PATCH #update' do
+    context 'when successfully created' do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: { id: @user.id, email: 'new@email.com' }, format: :json
+      end
+
+      it 'renders user json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql('new@email.com')
+      end
+
+      it 'returns status code 200' do
+        expect(response.status).to eql(200)
+      end
+
+    end
+
+    context 'when not created' do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: { id: @user.id, email: 'email.com' }, format: :json
+      end
+
+      it 'renders error json' do
+        error_response = JSON.parse(response.body, symbolize_names: true)
+        expect(error_response).not_to be_nil
+      end
+
+      it 'returns status code 422' do
+        expect(response.status).to eql(422)
+      end
+    end
+  end
 end
