a1857675e5ce37006684bccf573ce6eb8f781e78
Mariana <mariana.ristovska@gmail.com>
2017-07-04 17:38:35 +0200

    Associate products and orders with a placements model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,4 +15,8 @@ RSpec.describe Product, type: :model do
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
 
   it { should belong_to :user }
+
+  it { should have_many :placements }
+  it { should have_many(:orders).through(:placements) }
+
 end
