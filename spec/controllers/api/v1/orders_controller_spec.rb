834145ce824a141296bd53d850e5813db3443fbd
Mariana <mariana.ristovska@gmail.com>
2017-07-04 17:55:41 +0200

    Added show action for orders

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -16,4 +16,19 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
 
     it { expect(response.status).to eql 200 }
   end
+
+  describe 'GET #show' do
+    before(:each) do
+      current_user = FactoryGirl.create :user
+      api_authorization_header current_user.auth_token
+      @order = FactoryGirl.create :order, user: current_user
+      get :show, params: { user_id: current_user.id, id: @order.id }
+    end
+
+    it 'returns the user order matching the id' do
+      expect(json_response[:id]).to eql @order.id
+    end
+
+    it { expect(response.status).to eql 200 }
+  end
 end
