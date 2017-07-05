22e98077cd639f57b39cfeb1eaf8117f69c731d4
Mariana <mariana.ristovska@gmail.com>
2017-07-05 13:38:29 +0200

    Fixed warnings for deprecated methods

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -3,31 +3,31 @@ require "rails_helper"
 RSpec.describe OrderMailer, type: :mailer do
   include Rails.application.routes.url_helpers
 
-  describe ".send_confirmation" do
+  describe '.send_confirmation' do
     before(:all) do
       @order = FactoryGirl.create :order
       @user = @order.user
       @order_mailer = OrderMailer.send_confirmation(@order)
     end
 
-    it "should be set to be delivered to the user from the order passed in" do
-      @order_mailer.should deliver_to(@user.email)
+    it 'should be set to be delivered to the user from the order passed in' do
+      expect(@order_mailer).to deliver_to(@user.email)
     end
 
-    it "should be set to be send from no-reply@marketplace.com" do
-      @order_mailer.should deliver_from('no-reply@marketplace.com')
+    it 'should be set to be send from no-reply@marketplace.com' do
+      expect(@order_mailer).to deliver_from('no-reply@marketplace.com')
     end
 
     it "should contain the user's message in the mail body" do
-      @order_mailer.should have_body_text(/Order: ##{@order.id}/)
+      expect(@order_mailer).to have_body_text(/Order: ##{@order.id}/)
     end
 
-    it "should have the correct subject" do
-      @order_mailer.should have_subject(/Order Confirmation/)
+    it 'should have the correct subject' do
+      expect(@order_mailer).to have_subject(/Order Confirmation/)
     end
 
-    it "should have the products count" do
-      @order_mailer.should have_body_text(/You ordered #{@order.products.count} products:/)
+    it 'should have the products count' do
+      expect(@order_mailer).to have_body_text(/You ordered #{@order.products.count} products:/)
     end
     end
 end
